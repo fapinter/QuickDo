85c6c82ae9a43a2fabaa3fc5685d2e518a46ee85
fapinter <fpinterich@hotmail.com>
2026-01-03 00:31:02 -0300

    Functions finished, invoking and testing rn

--- handlers.go
+++ handlers.go
@@ -28,55 +28,82 @@ func InitDB(filepath string) *sql.DB {
 	);`
 	_, err_table_create := db.Exec(sql_script);
 	if err_table_create != nil {
-		log.Fatalf("Erro ao criar tabela: %q: %s\n", err_table_create, sql_script)
+		log.Fatalf("Erro ao criar tabela: %s: %s\n", err_table_create, sql_script)
 	}
 	return db
 }
 
 // Function to add tasks into the Database
 func AddTask(db *sql.DB, tasks[]string){
-
-	var sql_script string = "INSERT INTO tasks(text_todo, due_date) VALUES"
+	var sql_script string = "INSERT INTO todo_items(text_todo, due_date) VALUES"
 	var tasks_inserted []string
-
-	for i := 0; i < len(tasks); i++ {
+	for _, value := range tasks {
 		var date, text_todo string
 			
-		text_todo = tasks[i]
-		re := regexp.MustCompile(":(\d{4}-\d{2}-\d{2})")
-		dates := re.FindAll(text_todo)
-
+		text_todo = value
+		re := regexp.MustCompile(`:(\d{4}-\d{2}-\d{2})`)
+		var dates []string = re.FindAllString(text_todo, -1)
+		//Makes sure to get the last one if a Date is found
 		if len(dates) > 0 {
 			date = dates[len(dates) - 1]
 			text_todo = strings.TrimSuffix(text_todo, date)
-		} else {
-			date = "NULL"
 		}
-		var temp string = "(" + text_todo + "," + date + ")"
-		tasks_inserted.append(temp)
+		var temp string = fmt.Sprintf("('%s', '%s')", text_todo, date)
+		tasks_inserted = append(tasks_inserted, temp)
 	}
-	for task_inserted {
-
-	}
-	_, err_insert := db.Exec(sql_script, text_todo, due_date)
+	sql_script += strings.Join(tasks_inserted, ",")
+	_, err_insert := db.Exec(sql_script)
 	if err_insert != nil {
 		log.Fatal(err_insert)
 	}
 
-	fmt.Printf("Task '%s' added succesfully!\n", len(tasks))
-	if due_date != "" {
-		fmt.Printf("\tDue Date: %s", due_date)
-	}
+	fmt.Printf("Task '%d' added successfully!\n", len(tasks))
 }
 
-func ListTasks(db *sql.DB) {
-	rows, err_list := db.Query("SELECT todo_id, text_todo, complete, due_date FROM todo_items;")
+func ListTasks(db *sql.DB, cap int) {
+	var sql_script string = "SELECT todo_id, text_todo, complete, due_date FROM todo_items"
+	if cap > 0 {
+		sql_script += fmt.Sprintf(" LIMIT %d", cap)
+	}
+	rows, err_list := db.Query(sql_script)
 	if err_list != nil {
 		log.Fatal(err_list)
 	}
 	defer rows.Close()
-
+	
+	type Task struct {
+		id				int
+		text  		string
+		completed bool
+		due_date  string
+		expired   bool
+	}
+	
 }
+
+func UpdateTask(db *sql.DB, id int, text string, date string) {
+	var (
+		query string = "UPDATE todo_items SET "
+		err error
+	)
+	if text != "" && date != "" {
+		query += "text_todo=?, due_date=? WHERE todo_id=?"
+		_, err = db.Exec(query, text, date, id)
+	} else if text != "" {
+		query += "text_todo=? WHERE todo_id=?"
+		_, err = db.Query(query, text, id)
+	}else{
+		query += "due_date=? WHERE todo_id=?"	
+		_, err = db.Query(query, date, id)
+	}
+	if err != nil {
+		log.Fatal(err)
+	}else {
+		fmt.Println("Task updated successfully")
+	}
+}
+
+
 func RemoveTask(db *sql.DB, task_ids []string) {
 	var query string = ` DELETE FROM todo_items WHERE todo_id IN (`;
 	query += strings.Join(task_ids, ", ")
@@ -85,7 +112,7 @@ func RemoveTask(db *sql.DB, task_ids []string) {
 	if err != nil {
 		log.Fatal(err)
 	} else {
-		fmt.Println("Task removed successfully")
+		fmt.Printf("%d Task(s) removed successfully", len(task_ids))
 	}
 
 }
@@ -101,5 +128,5 @@ func CleanTasks(db *sql.DB) {
 	for rows.Next() {
 		deleted_rows++
 	}
-	fmt.Printf("%v deleted tasks\n", deleted_rows)
+	fmt.Printf("%d deleted tasks\n", deleted_rows)
 }
--- main.go
+++ main.go
@@ -2,13 +2,39 @@ package main
 
 
 import (
-	"fmt"
-	
+	"os"
+	"strconv"
+	"log"
 )
 
 const DATABASE_PATH = "./todos.db"
 
 func main() {
-	InitDB(DATABASE_PATH)
-	fmt.Print("Database initiated")
+	db := InitDB(DATABASE_PATH)
+
+ 	var op string = os.Args[1]
+
+	switch op {
+		case "add":
+			var tasks []string = os.Args[2:]
+			AddTask(db, tasks)
+
+		case "list":
+			if len(os.Args) == 3 {
+				cap, err := strconv.Atoi(os.Args[2])
+			}
+			if err != nil {
+				log.Fatal(err)
+			} else {
+				ListTasks(db, cap)
+			}
+
+		case "update":
+
+		case "remove":
+
+		case "cleanup":
+
+		case "export":
+	}
 }
